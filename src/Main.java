62d8d477e8ced1fae141a3aad050dc026403d682
Александр <kuzya-79@yandex.ru>
2022-08-13 11:43:34 +0300

    Сделано дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Name1", "Surname1");
+        Author author2 = new Author("Name2", "Surname2");
+
+        Book book1 = new Book("Title1", 2020, author1);
+        Book book2 = new Book("Title2", 2021, author2);
+
+
+        System.out.println("Имя автора: " + author1.getName() + ", Фамилия автора " + author1.getName());
+        System.out.println("Имя автора: " + author2.getName() + ", Фамилия автора " + author2.getName());
+        System.out.println("Название книги: " + book1.getName() + ", Год издания книги " + book1.getYear() + ", Автор книги: " + book1.getAuthor().getName());
+        System.out.println("Название книги: " + book2.getName() + ", Год издания книги " + book2.getYear() + ", Автор книги: " + book2.getAuthor().getName());
     }
 }
