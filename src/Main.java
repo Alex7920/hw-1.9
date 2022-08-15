892a05d9a80632b03a018cb13cc3941cacbe97da
Александр <kuzya-79@yandex.ru>
2022-08-15 10:42:53 +0300

    Исправил замечание

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
 
+
+
+
     public static void main(String[] args) {
-        Author author1 = new Author("Name1", "Surname1");
-        Author author2 = new Author("Name2", "Surname2");
 
-        Book book1 = new Book("Title1", 2020, author1);
-        Book book2 = new Book("Title2", 2021, author2);
+        Author author1 = new Author("Антон", " Чехов");
+        Author author2 = new Author("Николай", " Гоголь");
 
 
-        System.out.println("Имя автора: " + author1.getName() + ", Фамилия автора: " + author1.getName());
+        Book book1 = new Book("Человек в футляре", 1898, author1);
+        Book book2 = new Book("Ревизор", 1836, author2);
+
+
+        System.out.println("Имя автора: " + author1.getName() + ", Фамилия автора: " + author1.getSurname());
         System.out.println("Имя автора: " + author2.getName() + ", Фамилия автора: " + author2.getName());
-        System.out.println("Название книги: " + book1.getName() + ", Год издания книги: " + book1.getYear() + ", Автор книги: " + book1.getAuthor().getName());
-        System.out.println("Название книги: " + book2.getName() + ", Год издания книги: " + book2.getYear() + ", Автор книги: " + book2.getAuthor().getName());
+        System.out.println("Название книги: " + book1.getName() + ", Год издания книги: " + book1.getYear() + ", Автор книги: " + book1.getAuthor().getName() + author1.getSurname());
+        System.out.println("Название книги: " + book2.getName() + ", Год издания книги: " + book2.getYear() + ", Автор книги: " + book2.getAuthor().getName() + author2.getSurname());
     }
 
 
